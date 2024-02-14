10c706d13d9594da7481ce0f555805c48d3734e6
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-02-14 13:05:33 +0100

    Remplissage primaire du Footer

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -5,6 +5,24 @@ import PropTypes from 'prop-types';
 export default function Footer() {
     return (
         <footer className="full-width">
+            <div className="container-links-news">
+                <div className="container-links">
+                    liens divers
+                </div>
+
+                <div className="newsletter">
+                    <p>Newsletter</p>
+                    <input className="full-width" type="text" placeholder="Votre adresse e-mail"/>
+                    <a className="btn button-primary full-width">S'inscrire</a>
+                </div>
+            </div>
+
+            <div className="container-infos">
+                <p className="infos-title">WildWonderHub, application de gestion de zoo depuis 2023</p>
+                <p className="copyright">
+                    © Copyright 2023 - All Rights Reserved by WildWonderHub.
+                </p>
+            </div>
         </footer>
     )
 }
