efcf9d2ea8769a5795d44eb9e72f5f3c56ac870e
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-02-14 12:50:16 +0100

    Ajout du Footer vide

---
+++ src/components/page/Footer.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+
+// props => {}
+export default function Footer() {
+    return (
+        <footer className="full-width">
+        </footer>
+    )
+}
+
+Footer.PropTypes = {
+}
+
+Footer.defaultProps = {
+}
