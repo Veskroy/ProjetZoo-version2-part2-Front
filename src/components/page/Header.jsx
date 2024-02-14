f6b8ecee0d44b7c5e6b0b54c4c0ac1c8f6979709
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-02-14 13:05:24 +0100

    Remplissage primaire du Header

--- src/components/page/Header.jsx
+++ src/components/page/Header.jsx
@@ -4,6 +4,14 @@ import PropTypes from 'prop-types';
 export default function Header() {
     return (
         <header className="header">
+            <div className="main-infos">
+                Zoo de la Palmyre
+            </div>
+            <nav className="ul-links">
+                <div>
+                liens des boutons
+                </div>
+            </nav>
         </header>
     )
 }
