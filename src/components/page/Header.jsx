eb88180d4f5daf1efd4346fee6beb349893fbe3e
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-02-14 12:50:24 +0100

    Ajout du Header vide

---
+++ src/components/page/Header.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+// props => {}
+export default function Header() {
+    return (
+        <header className="header">
+        </header>
+    )
+}
+
+Header.PropTypes = {
+}
+
+Header.defaultProps = {
+}
