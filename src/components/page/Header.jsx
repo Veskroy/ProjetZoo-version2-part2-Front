755751efb09afed55bf310f772006f2976cc2c8a
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-17 15:11:29 +0100

    add: liens au composant Header

--- src/components/page/Header.jsx
+++ src/components/page/Header.jsx
@@ -1,4 +1,9 @@
-import PropTypes from 'prop-types';
+/* eslint-disable react/no-unescaped-entities */
+// import PropTypes from 'prop-types';
+
+import { Link } from "wouter";
+
+import { loginUrl, registerUrl } from "../../services/api/WildWonderHub";
 
 // props => {}
 export default function Header() {
@@ -12,9 +17,11 @@ export default function Header() {
             </div>
             <nav className="ul-links">
                 <div>
-                    <a href="" className="menu-link">Accueil</a>
+                    <Link href="/" className="menu-link">Accueil</Link>
                     <a href="" className="menu-link">Liste des animaux</a>
                     <a href="" className="menu-link">Evènements</a>
+                    <a href={loginUrl()} className="btn button-secondary">Se connecter</a>
+                    <a href={registerUrl()} className="btn button-primary">S'inscrire</a>
                 </div>
             </nav>
         </header>
