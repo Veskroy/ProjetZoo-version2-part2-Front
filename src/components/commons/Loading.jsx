4c774fb3289a737dfda6a7660ff171d142ddacb4
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-02-21 12:58:19 +0100

    Amélioration de l'apparence du loading

--- src/components/commons/Loading.jsx
+++ src/components/commons/Loading.jsx
@@ -3,12 +3,14 @@ import '/public/assets/styles/loading.css'
 export default function Loading() {
     return (
         <div className="loader-container">
-            <div className="pl2">
-                <div className="pl2__a"></div>
-                <div className="pl2__b"></div>
-                <div className="pl2__c"></div>
+            <div>
+                <div className="pl2">
+                    <div className="pl2__a"></div>
+                    <div className="pl2__b"></div>
+                    <div className="pl2__c"></div>
+                </div>
+                <div className="loader">Loading...</div>
             </div>
-            <div className="loader">Loading...</div>
         </div>
     );
 }
