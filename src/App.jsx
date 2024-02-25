803e7b9d210d7c0c97614b40c4db4270dd3e9613
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-25 20:12:12 +0100

    edit: nettoyage du code et utilisation du provider d'utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useEffect } from "react"
 import { useLocation } from "wouter";
 import Router from "./routes";
 
-import { testGetLoggedUser } from "./services/api/WildWonderHub"
-
 import BasePage from './components/page/BasePage';
+import UserProvider from "./context/user/UserProvider";
 
 function App() {
 
-  useEffect(() => {
-    const testApiConnection = async () => {
-      try {
-        const response = await testGetLoggedUser();
-        console.log('réponse API :', response);
-      } catch (error) {
-        console.error('erreur connexion API :', error);
-      }
-    };
-
-    testApiConnection();
-  }, []);
-
   const [location] = useLocation();
-  console.log(location);
   
   return (
     <>
-      <BasePage className={`${location === '/' ? 'homepage' : 'content'}`}>
-        <Router />
-      </BasePage>
+      <UserProvider>
+        <BasePage className={`${location === '/' ? 'homepage' : 'content'}`}>
+          <Router />
+        </BasePage>
+      </UserProvider>
     </>
   )
 }
