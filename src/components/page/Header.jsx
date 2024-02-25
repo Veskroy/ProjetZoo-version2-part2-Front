00f08e08ec468423c8907d4702d822936fbe69d6
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-25 20:11:30 +0100

    add: liens du menu en fonction de l'utilisateur

--- src/components/page/Header.jsx
+++ src/components/page/Header.jsx
@@ -2,10 +2,16 @@
 
 import { Link } from "wouter";
 
-import { loginUrl, registerUrl } from "../../services/api/WildWonderHub";
+import { loginUrl, logoutUrl, registerUrl } from "../../services/api/WildWonderHub";
+import { useAccount, useRolesUser } from "../../hooks/getAccount";
 
 // props => {}
 export default function Header() {
+    const { userContext, isLoggedIn, errorLogin } = useAccount();
+    const { isAdmin, isUser, isEmployee } = useRolesUser();
+
+    console.log('roles from header: ', isAdmin, isUser, isEmployee);
+    console.log('user from header: ', userContext, isLoggedIn);
     return (
         <header className="header">
             <div className="main-infos">
@@ -19,8 +25,22 @@ export default function Header() {
                     <Link href="/" className="menu-link">Accueil</Link>
                     <a href="" className="menu-link">Liste des animaux</a>
                     <a href="" className="menu-link">Evènements</a>
-                    <a href={loginUrl()} className="btn button-secondary">Se connecter</a>
-                    <a href={registerUrl()} className="btn button-primary">S'inscrire</a>
+
+                    {isLoggedIn && userContext && !errorLogin ? (
+                        <>
+                            <a href={loginUrl()} className="btn button-secondary">Réserver</a>
+                            <a href={logoutUrl()} className="btn button-primary">Se déconnecter</a>
+                            {isAdmin && (
+                                <Link href="/admin" className="btn button-admin">Admin</Link>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <a href={loginUrl()} className="btn button-secondary">Se connecter</a>
+                            <a href={registerUrl()} className="btn button-primary">S'inscrire</a>
+                        </>
+                    )}
+
                 </div>
             </nav>
         </header>
