6adc0d5dd59b01e78e81ad9c2e8c723876121b86
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-03-16 15:47:21 +0100

    add: vue liée au forum ajoutée

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,32 @@
 import { Route, Switch } from "wouter";
+import PropTypes from 'prop-types';
+
 import NotFound from "../views/NotFound";
 import Politiques from "../views/Politiques";
 import Home from "../views/Home";
+import Forum from "../views/Forum";
 
-function Router() {
+function Router({ page, setPage }) {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/politiques" component={Politiques} />
+      <Route path="/forum">
+        <Forum page={page} setPage={setPage} />
+      </Route>
       <Route component={NotFound} />
     </Switch>
   );
 }
 
 export default Router;
+
+Router.propTypes = {
+  page: PropTypes.number.isRequired,
+  setPage: PropTypes.func.isRequired
+}
+
+Router.defaultProps = {
+  page: '',
+  setPage: () => {}
+}
