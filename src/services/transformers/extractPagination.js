bcdf8902c520614cf2ff16a8cda49529f3e640a6
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-03-16 15:45:29 +0100

    add: fonction pour extraire les données de Pagination

---
+++ src/services/transformers/extractPagination.js
@@ -0,0 +1,27 @@
+function convertToUrlParams(url) {
+    if (url === null || url === undefined) return null;
+    console.log("URLSearchParams", new URLSearchParams(url));
+    return url.split('?')[1];
+}
+
+export function extractPagination(hydraView) {
+    const current = parseInt(new URLSearchParams(convertToUrlParams(hydraView['@id'])).get('page'), 10);
+    const first = parseInt(new URLSearchParams(convertToUrlParams(hydraView['hydra:first']))?.get('page'), 10);
+    const previous = parseInt(new URLSearchParams(convertToUrlParams(hydraView['hydra:previous']))?.get('page'), 10);
+    const next = parseInt(new URLSearchParams(convertToUrlParams(hydraView['hydra:next']))?.get('page'), 10);
+    const last = parseInt(new URLSearchParams(convertToUrlParams(hydraView['hydra:last']))?.get('page'), 10);
+  
+    if (current === undefined || current === null) {
+      return null;
+    }
+ 
+    console.log(current, first, previous, next, last);
+
+    return {
+      current,
+      first,
+      previous,
+      next,
+      last,
+    };
+}
