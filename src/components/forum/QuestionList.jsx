7e8dc0b7e24bda054914df35a51364d3fd9a1736
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-03-18 15:12:22 +0100

    refactor: vues, composants liés au forum

--- src/components/forum/QuestionList.jsx
+++ src/components/forum/QuestionList.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import QuestionItem from './QuestionItem'
 
-export default function QuestionList({ page, data }) {
-    console.log('forumData from list', data);
+export default function QuestionList({ page, data, children }) {
     return (
         <div className="faq-container">
             <h2 className="second-title mt-50 mb-50">Les posts les plus récents (page {page})</h2>
+
+            {children}
             
             <div className="questions-container">
                 {data.map((question, index) => (
@@ -18,10 +19,12 @@ export default function QuestionList({ page, data }) {
 
 QuestionList.propTypes = {
     data: PropTypes.array.isRequired,
-    page: PropTypes.number.isRequired
+    page: PropTypes.number.isRequired,
+    children: PropTypes.node
 }
 
 QuestionList.defaultProps = {
     data: [],
-    page: 1
+    page: 1,
+    children: null
 }
