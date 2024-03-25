d448fa163e6b882c22acc2b4752b2d359e161c98
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2024-03-25 13:36:27 +0100

    Ajout de la page règlement

---
+++ src/views/Reglement.jsx
@@ -0,0 +1,102 @@
+function Reglement() {
+    return (
+        <><h1 className="main-title">
+            Règlement du Zoo
+        </h1><div className="reglement">
+            <h5>Article 1 : AUTORISATION D’ENTRÉE</h5>
+            <p>Seul le visiteur porteur d’un billet numéroté, dénommé « Certificat de Visite », délivré à l’entrée de l’Établissement pourra pénétrer dans le Parc Zoologique.
+            La validité de ce billet n’est prévue que pour la durée de la journée.
+            En aucun cas, la remise par un visiteur à l’entrée de l’Établissement d’un billet préalablement détaché ne permettra l’accès au Parc Zoologique.</p>
+
+            <h5>Article 2 : COMPORTEMENT DES VISITEURS</h5>
+            <p>Le visiteur devra pénétrer dans le Parc Zoologique dans une tenue convenable et décente.
+            Il devra adopter une attitude normale de nature à ne pas exciter, effrayer ou tourmenter les animaux.
+            Il évitera de courir et de poursuivre les animaux domestiques ou domestiqués en liberté.
+            Il veillera à modérer le ton de sa voix, et ne pourra se livrer à aucune exhibition ou imitation des cris ou des comportements des animaux.
+            Il ne pourra donner de la nourriture aux animaux, sauf en la présence et avec l’accord d’un responsable attaché à la Direction.
+            Il ne devra ni jeter de projectile vers les animaux, ni leur présenter d’objet.
+            Il devra respecter les panneaux d’information ou d’interdiction.</p>
+
+            <h5>Article 3 : SURVEILLANCE DES ENFANTS</h5>
+            <p>Les enfants devront rester, dès l’entrée dans le Parc Zoologique, sous la surveillance continuelle des parents ou accompagnateurs qui en auront l’entière responsabilité.
+            L’encadrement des groupes d’enfants est obligatoire et doit comprendre au minimum un accompagnateur pour quinze enfants.
+            Pour des raisons de sécurité, la Direction se réserve la possibilité de refuser la visite du Parc Zoologique à de jeunes enfants non accompagnés se présentant à la Caisse. La décision de renvoi de trop jeunes enfants pouvant dans certains cas engager la responsabilité de cet Établissement, il est prévu que ces derniers seront retenus par la Direction en attendant d’être mis à la disposition des autorités de Police ou de Gendarmerie locales qui auront été immédiatement prévenues.
+            Dans le cas où des enfants non accompagnés auraient pénétré à l’insu de la Direction sur le périmètre dépendant du Parc Zoologique, sur son parking, ses abords ou sur les « Réserves », l’Établissement ne saurait être mis en cause dans tout accident ou difficultés quelconques qui surviendraient.</p>
+
+            <h5>Article 4 : PÉNÉTRATION DU PUBLIC DANS LES LOCAUX DE SERVICE</h5>
+            <p>Il est formellement interdit, à toute personne, de pénétrer dans les locaux affectés au service, les bâtiments d’exploitation, ainsi que d’emprunter véhicules et matériels appartenant au Parc Zoologique, sauf dans les cas présentant un caractère d’urgence, comme fuite d’un animal dangereux, incendie, accident ou tout événement de même nature.</p>
+
+            <h5>Article 5 : CIRCULATION DES VÉHICULES</h5>
+            <p>Seuls les véhicules de service propres à l’Établissement pourront circuler en tout endroit du Parc Zoologique, voies intérieures et pourtours.</p>
+
+            <h5>Article 6 : SURVEILLANCE DES BIENS DES VISITEURS DANS LE PARC ZOOLOGIQUE</h5>
+            <p>Aucun personnel de surveillance ou aucune disposition spéciale, en matière de vol, ne sont prévus dans le dit Établissement. La Direction décline toute responsabilité :
+
+            en cas de disparition d’objets, de vols qui pourraient être commis par des visiteurs ou par son personnel salarié ;
+            en cas d’accidents ou de dommages résultant de difficultés, ou de heurts entre visiteurs.
+            En cas de vol, la victime devra immédiatement en informer la Direction, mais fera son affaire personnelle des formalités et recours qu’elle jugera utiles et nécessaires d’entreprendre. Pour toute perte d’objets, la déclaration sera également faite dans les plus brefs délais à la « Caisse » ou au « Bureau ». Toute somme d’argent ou objet de valeur trouvés dans le Parc Zoologique et remis à la Direction sera</p>
+
+            <h5>Article 7 : DEMANDE DE RENSEIGNEMENTS – ENREGISTREMENT DE RÉCLAMATIONS ET DE DÉPÔT DE DÉCLARATION</h5>
+            <p>Les visiteurs pourront demander les renseignements dont ils ont besoin, soit à la Caisse, soit auprès du personnel et des commerces se trouvant dans le Parc Zoologique. En cas de difficultés, alerter ces mêmes personnes. La caisse, le bureau, les commerces sont en liaison constante avec la Direction.
+            Toute réclamation, observation ou dépôt de déclaration verbale ou écrite, de quelque nature que ce soit, sera également enregistrée au bureau ; des registres étant prévus à cet effet, notamment pour l’enregistrement des pertes et vols et documents de décharge lors des restitutions.</p>
+
+            <h5>Article 8 : AUTORISATION D’UTILISER DES APPAREILS PHOTOGRAPHIQUES</h5>
+            <p>L’usage par les visiteurs d’appareils de prise de vues et d’enregistrement est possible. Toutefois les photographies, films et enregistrements ainsi réalisés ne devront en aucun cas être utilisés à des fins commerciales et présentations publiques, même à titre gratuit, sauf dérogations expresses.
+            En ce qui concerne la peinture et le dessin, un accord préalable doit être donné par la Direction, qui ne dispense pas de l’acquittement du billet délivré à l’entrée.</p>
+
+            <h5>Article 9 : BAR, CONFISERIE, AIRE DE PIQUE-NIQUE, etc…</h5>
+            <p>Dans le Parc Zoologique, un bar, une crêperie et des stands de glaces sont mis à la disposition des visiteurs.
+            Les tabourets et tables des terrasses du bar et de la crêperie sont réservés aux consommateurs. Le pique-nique est autorisé dans le Parc Zoologique sur les gradins des otaries et perroquets, et sur les bancs situés dans les allées, sous réserve de laisser les lieux propres.</p>
+
+            <h5>Article 10 : TOILETTES ET WC</h5>
+            <p>Conformément aux dispositions législatives en vigueur, des locaux à usage de toilettes et WC sont mis à la disposition des visiteurs. L'eau des toilettes n’est pas une eau « boisson ».</p>
+
+            <h5>Article 11 : SECOURS D’URGENCE</h5>
+            <p>Des premiers soins d’urgence peuvent être donnés par une personne faisant partie de l’Établissement, ayant reçu une formation de secouriste.
+            Au bureau situé près de l’entrée et au bar situé près du bassin des otaries, des soins d’urgence peuvent être dispensés aux personnes blessées.
+            Pour les personnes victimes de malaises : alerter le personnel des commerces qui par talkie-walkie préviendra les personnes habilitées à vous aider. Un brancard ou un fauteuil roulant pourront être mis à votre disposition pour rejoindre l’entrée du Parc Zoologique.
+            A votre demande, un médecin ou les pompiers seront appelés immédiatement. L’arrivée des pompiers avec un véhicule de premiers soins survient entre 4 à 6 minutes après notre appel téléphonique.
+            Si un animal dangereux était en fuite, outre l’alerte donnée et la Direction prévenue, il est conseillé de se réfugier dans tout local de service, endroit ou véhicule offrant une sécurité.
+            Une sonorisation générale permet de prévenir l’ensemble des visiteurs dans des cas de force majeure.</p>
+
+            <h5>Article 12 : PLAN DU PARC ZOOLOGIQUE - PARKING ET VOIES D’ACCÈS</h5>
+            <p>Le plan de l’ensemble du Parc Zoologique a été réalisé et comprend une légende. Il mentionne en outre « Caisse », « Bureau », « Commerces ». Plusieurs de ces plans sont situés sur le parcours de visite du Parc Zoologique.</p>
+
+            <h5>Article 13 : SUPPRESSIONS TEMPORAIRES OU DÉFINITIVES DES ESPÈCES ANIMALES PRÉSENTÉES</h5>
+            <p>La Direction du Parc Zoologique se réserve à tout moment la possibilité de modifier, de supprimer temporairement ou définitivement la présentation de certains animaux ou des spectacles pour des raisons qui lui sont propres (naissance, décès, quarantaine), sans pour cela permettre au public d’adresser des réclamations écrites ou orales, ou être dans l’obligation de restituer tout ou partie du montant du billet d’entrée.</p>
+
+            <h5>Article 14 : FERMETURE ET ÉVACUATION DU PARC ZOOLOGIQUE DUES AUX INTEMPÉRIES</h5>
+            <p>En cas d’intempéries survenues au cours d’une visite ou préalablement à l’ouverture de l’Établissement (comme grêle, tempête, chute de neige, etc.) le Parc Zoologique sera immédiatement fermé ou évacué ; dans ce dernier cas, le public aura une autorisation pour effectuer une visite ultérieure.</p>
+
+            <h5>Article 15 : SANCTIONS ET APPEL AUX AGENTS DE LA FORCE PUBLIQUE</h5>
+            <p>En cas de non-respect par le public des dispositions du présent Règlement Intérieur, des normes propres au parking ainsi qu’en cas de troubles ou incidents créés par les visiteurs, la Direction peut procéder ou faire procéder par les agents de la force publique à l’expulsion des contrevenants dans la mesure où ces derniers auraient refusé de quitter volontairement l’Établissement.
+            S’il y a lieu, la Direction devra procéder à des constats par Huissier de Justice et des témoignages pourront être recueillis auprès des visiteurs.</p>
+
+            <h5>Article 16 : CIRCULATION DU PUBLIC DANS LE PARC ZOOLOGIQUE</h5>
+            <p>Pour permettre au public de visiter le Parc Zoologique, il a été aménagé des allées goudronnées, délimitées par des garde-corps devant les cages des animaux reconnus dangereux, soit par des mains courantes en bois ou métalliques.
+            Les enclos et autres voies d’accès sont exclusivement réservés au personnel et à la Direction du Parc Zoologique. Les panneaux d’information ou d’interdiction devront être respectés.
+            Les espaces de sécurité sont délimités par des barrières empêchant tout contact entre le public et l’animal ; l’escalade, le franchissement et le passage des doigts à travers ces clôtures et cages sont formellement interdits.
+            Aucun cycle, aucun véhicule à moteur ne pourra être utilisé au cours de la visite du Parc Zoologique.
+            Aucun produit dangereux, aucune arme, aucun poste de radio ou instrument sonore ne pourra être introduit dans le Parc Zoologique.
+            Aucune démonstration publicitaire ni aucune distribution d’imprimés ou affichage ne sont autorisés, pas plus que l’entrée de marchands ambulants ou de mendiants.</p>
+
+            <h5>Article 17 : ACCÈS DANS LE PARC D’ANIMAUX APPARTENANT AU PUBLIC</h5>
+            <p>L’entrée dans le Parc d’animaux appartenant aux visiteurs ne pourra être autorisée en raison des risques de contamination et des dégâts ou désagréments que ceux-ci pourraient entraîner.</p>
+
+            <h5>Article 18 : LUTTE CONTRE L’INCENDIE</h5>
+            <p>En cas d’incendie dans le massif forestier, la Direction, en relation avec les pompiers, informera les visiteurs et décidera l’évacuation du Parc Zoologique si nécessaire.
+            En cas de début d’incendie dans le Parc Zoologique, nous faisons appel au civisme des visiteurs pour alerter les postes « Caisse », « Bureau », « Commerce » et dans la mesure de leurs possibilités, sur un incendie naissant d’essayer de le circonscrire.
+            En cas de danger imminent, tout le personnel du Parc Zoologique recevra par talkie-walkie les consignes d’intervention et notamment l’ouverture des portails de services donnant accès aux parkings.</p>
+
+            <h5>Article 19 : RÈGLES PROPRES AUX PARKINGS ET VOIES D’ACCÈS AU PARC ZOOLOGIQUE</h5>
+            <p>Les parkings et ses voies d’accès, situés hors de l’enceinte du Parc Zoologique, font l’objet de prescriptions et normes différentes de celles comprises dans le présent règlement.
+            Les voies d’accès et les parkings sont réservés exclusivement aux visiteurs du Parc. Les conditions de circulation des véhicules sont définies par l’ensemble des règlements du Code de la Route.
+            Le stationnement ne peut être que temporaire et correspondre aux visites ; il doit s’effectuer correctement et dans un bon ordre afin de laisser les voies de dégagement nécessaires pour l’entrée et la sortie des véhicules.
+            Ces parkings gratuits ne font l’objet d’aucune surveillance et la Direction ne peut être tenue pour responsable des vols ou accidents qui pourraient y survenir.
+            Les parkings ne peuvent être utilisés : comme terrain de jeux pour les enfants, comme aire de repos, comme lieu de promenade à bicyclette ou motocyclette.</p>
+        </div>
+        </>
+    )
+}
+
+export default Reglement;
