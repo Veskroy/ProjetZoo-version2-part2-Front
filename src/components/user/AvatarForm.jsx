125b8b3ac4e15b722add17c99ba4ab14a96a75bf
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-03-29 09:40:26 +0100

    add: formulaire pour modifier l'avatar

---
+++ src/components/user/AvatarForm.jsx
@@ -0,0 +1,35 @@
+import Element from "../commons/Form/Element";
+import Form from "../commons/Form/Form";
+
+import PropTypes from 'prop-types';
+
+export default function AvatarForm({ handleAvatarSubmit, loading }) {
+    let textElement = loading ? 'Chargement...' : 'Modifier mon avatar';
+    return (
+        <Form className="avatar-form" onSubmit={handleAvatarSubmit}>
+            <Element input={{
+                type: 'label',
+                text: 'Ma photo de profil',
+            }} />
+            <Element input={{
+                type: 'file',
+            }} />
+            <Element input={{
+                type: 'submit',
+                text: textElement,
+                class: 'btn button-primary full-width',
+                onSubmit: handleAvatarSubmit,
+            }} />
+        </Form>
+    )
+}
+
+AvatarForm.propTypes = {
+    handleAvatarSubmit: PropTypes.func.isRequired,
+    loading: PropTypes.bool,
+}
+
+AvatarForm.defaultProps = {
+    handleAvatarSubmit: () => {},
+    loading: false,
+}
